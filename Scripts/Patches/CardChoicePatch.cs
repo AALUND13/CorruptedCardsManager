7adda9ccc27dac4e731f23b24745303a302b5398
AALUND13 <tamlund1976@gmail.com>
2025-05-25 16:50:23 -0400

    Make `CorruptedCardSpawnChancePerPick` actually work

---
+++ Scripts/Patches/CardChoicePatch.cs
@@ -0,0 +1,13 @@
+﻿using CorruptedCardsManager.Extensions;
+using HarmonyLib;
+
+namespace AALUND13Card.Patches {
+    [HarmonyPatch(typeof(CardChoice))]
+    public class CardChoicePatch {
+        [HarmonyPatch("StartPick")]
+        private static void Prefix(int pickerIDToSet) {
+            Player player = PlayerManager.instance.players[pickerIDToSet];
+            player.data.GetAdditionalData().CorruptedCardSpawnChance += player.data.GetAdditionalData().CorruptedCardSpawnChancePerPick;
+        }
+    }
+}
