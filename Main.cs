39db6ccb0f763bace62ed2d110cab2ea5c8cdf1c
AALUND13 <tamlund1976@gmail.com>
2025-05-25 16:49:16 -0400

    Added support for `TabInfo`

--- Main.cs
+++ Main.cs
@@ -1,6 +1,10 @@
 using BepInEx;
 using BepInEx.Logging;
+using CorruptedCardsManager.Extensions;
 using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace CorruptedCardsManager {
@@ -10,32 +14,38 @@ namespace CorruptedCardsManager {
     [BepInDependency("com.aalund13.rounds.random_cards_generator", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("root.rarity.lib", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("com.CrazyCoders.Rounds.RarityBundle", BepInDependency.DependencyFlags.HardDependency)]
+
+    [BepInDependency("com.willuwontu.rounds.tabinfo", BepInDependency.DependencyFlags.SoftDependency)]
+
     [BepInPlugin(modId, modName, "1.0.0")]
     [BepInProcess("Rounds.exe")]
     public class Main : BaseUnityPlugin {
         private const string modId = "com.aalund13.rounds.corrupted_manager";
         private const string modName = "Corrupted Manager";
         internal const string modInitials = "CCM";
-        
+
         internal static Main instance;
         internal static AssetBundle assets;
         internal static ManualLogSource ModLogger;
         internal static GameObject corruptedCardFancyIconPrefab;
 
 
-        void Awake() {
+        private void Awake() {
             instance = this;
             ModLogger = Logger;
 
             new Harmony(modId).PatchAll();
 
             assets = Jotunn.Utils.AssetUtils.LoadAssetBundleFromResources("corruptedmanager_assets", typeof(Main).Assembly);
-            corruptedCardFancyIconPrefab = assets.LoadAsset<GameObject>("I_Corrupted");     
+            corruptedCardFancyIconPrefab = assets.LoadAsset<GameObject>("I_Corrupted");
 
             Debug.Log($"{modName} loaded!");
         }
 
-        void Start() {
+        private void Start() {
+            var Plugins = (List<BaseUnityPlugin>)typeof(BepInEx.Bootstrap.Chainloader).GetField("_plugins", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+            if(Plugins.Exists(plugin => plugin.Info.Metadata.GUID == "com.willuwontu.rounds.tabinfo"))
+                TabinfoInterface.Setup();
             CorruptedCardsGenerators.Init();
         }
     }
---
+++ Scripts/TabinfoInterface.cs
@@ -0,0 +1,14 @@
+﻿using CorruptedCardsManager.Extensions;
+using TabInfo.Utils;
+
+namespace CorruptedCardsManager {
+    public class TabinfoInterface {
+        public static void Setup() {
+            var CCMStatsCategory = TabInfoManager.RegisterCategory("CCM Stats", 3);
+            TabInfoManager.RegisterStat(CCMStatsCategory, "Corrupted Cards Spawn Chance", (p) => p.data.GetAdditionalData().CorruptedCardSpawnChance != 0,
+                (p) => $"{p.data.GetAdditionalData().CorruptedCardSpawnChance * 100:0}%");
+            TabInfoManager.RegisterStat(CCMStatsCategory, "Corrupted Cards Spawn Chance Per Pick", (p) => p.data.GetAdditionalData().CorruptedCardSpawnChancePerPick != 0,
+                (p) => $"{p.data.GetAdditionalData().CorruptedCardSpawnChancePerPick * 100:0}%");
+        }
+    }
+}
