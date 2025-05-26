00b558039a5ddf344f873bfe7234fc53e702aad5
AALUND13 <tamlund1976@gmail.com>
2025-05-26 17:22:14 -0400

    Added `CorruptedCardSpawnChancePerFight` to `CorruptedStatModifers`

--- Scripts/CorruptedStatModifers.cs
+++ Scripts/CorruptedStatModifers.cs
@@ -5,6 +5,7 @@ namespace CorruptedCardsManager {
     public class CorruptedStatModifers : MonoBehaviour {
         public float CorruptedCardSpawnChance = 0f;
         public float CorruptedCardSpawnChancePerPick = 0f;
+        public float CorruptedCardSpawnChancePerFight = 0f;
 
         public void Apply(Player player) {
             CharacterData data = player.data;
@@ -12,6 +13,7 @@ namespace CorruptedCardsManager {
 
             additionalData.CorruptedCardSpawnChance = Mathf.Max(additionalData.CorruptedCardSpawnChance + CorruptedCardSpawnChance, 0f);
             additionalData.CorruptedCardSpawnChancePerPick += CorruptedCardSpawnChancePerPick;
+            additionalData.CorruptedCardSpawnChancePerFight += CorruptedCardSpawnChancePerFight;
         }
     }
 }
