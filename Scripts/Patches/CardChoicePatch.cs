546a06f504edf5bc6995e8672fb211937330c672
AALUND13 <tamlund1976@gmail.com>
2025-05-26 07:47:15 -0400

    Make the `AdditionalData` reset when the `CharacterData` reset

--- Scripts/Patches/CardChoicePatch.cs
+++ Scripts/Patches/CardChoicePatch.cs
@@ -2,9 +2,8 @@
 using HarmonyLib;
 
 namespace AALUND13Card.Patches {
-    [HarmonyPatch(typeof(CardChoice))]
+    [HarmonyPatch(typeof(CardChoice), "StartPick")]
     public class CardChoicePatch {
-        [HarmonyPatch("StartPick")]
         private static void Prefix(int pickerIDToSet) {
             Player player = PlayerManager.instance.players[pickerIDToSet];
             player.data.GetAdditionalData().CorruptedCardSpawnChance += player.data.GetAdditionalData().CorruptedCardSpawnChancePerPick;
---
+++ Scripts/Patches/CharacterStatModifiersPatch.cs
@@ -0,0 +1,13 @@
+﻿using CorruptedCardsManager.Extensions;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CorruptedCardsManager.Patches {
+    [HarmonyPatch(typeof(CharacterStatModifiers), "ResetStats")]
+    public class CharacterStatModifiersPatch {
+        public static void Prefix(CharacterStatModifiers __instance) {
+            CharacterData data = (CharacterData)Traverse.Create(__instance).Field("data").GetValue();
+            data.GetAdditionalData().Reset();
+        }
+    }
+}
