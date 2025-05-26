5fe95fa30efb977a3c312a50eb49b6f8e713ff78
AALUND13 <tamlund1976@gmail.com>
2025-05-26 17:21:54 -0400

    Fixed `ApplyCardStatsPatch` was using the wrong component

--- Scripts/Patches/ApplyCardStatsPatch.cs
+++ Scripts/Patches/ApplyCardStatsPatch.cs
@@ -4,7 +4,7 @@ namespace CorruptedCardsManager.Patches {
     [HarmonyPatch(typeof(ApplyCardStats), "ApplyStats")]
     public class ApplyCardStatsPatch {
         public static void Postfix(ApplyCardStats __instance, Player ___playerToUpgrade) {
-            CorruptedStatModifers corruptedStatModifers = ___playerToUpgrade.GetComponent<CorruptedStatModifers>();
+            CorruptedStatModifers corruptedStatModifers = __instance.GetComponent<CorruptedStatModifers>();
             if(corruptedStatModifers != null) {
                 corruptedStatModifers.Apply(___playerToUpgrade);
             }
