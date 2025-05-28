063fe11a91f08002fe3be812df21075090d9299d
AALUND13 <tamlund1976@gmail.com>
2025-05-28 14:01:30 -0400

    Fixed namespace naming

--- Scripts/Patches/CardChoicePatch.cs
+++ Scripts/Patches/CardChoicePatch.cs
@@ -1,12 +1,28 @@
 ﻿using CorruptedCardsManager.Extensions;
 using HarmonyLib;
+using ModdingUtils.Patches;
+using System;
+using System.Linq;
 
-namespace AALUND13Card.Patches {
+namespace CorruptedCardsManager.Patches {
     [HarmonyPatch(typeof(CardChoice), "StartPick")]
     public class CardChoicePatch {
         private static void Prefix(int pickerIDToSet) {
             Player player = PlayerManager.instance.players[pickerIDToSet];
             player.data.GetAdditionalData().CorruptedCardSpawnChance += player.data.GetAdditionalData().CorruptedCardSpawnChancePerPick;
+        }
+    }
+
+    [HarmonyPatch(typeof(CardChoicePatchGetRanomCard), "EfficientGetRanomCard", new Type[] { typeof(Player), typeof(CardInfo[]) })]
+    public class CardChoicePatchGetRanomCardPatch {
+        private static void Prefix(Player player, ref CardInfo[] cards) {
+            if(player == null || cards == null || cards.Length == 0)
+                return;
+
+            bool corruptedCardSpawnChance = UnityEngine.Random.Range(0f, 1f) < player.data.GetAdditionalData().CorruptedCardSpawnChance;
+            if(!corruptedCardSpawnChance) return;
+
+            cards = CorruptedCardsManager.DrawableCorruptedCards.Keys.ToArray();
         }
     }
 }
