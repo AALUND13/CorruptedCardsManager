e4b9b8094c73e809503b334e2b2e5a4a0a278ed7
AALUND13 <tamlund1976@gmail.com>
2025-05-30 20:43:30 -0400

    Move corrupted cards icon components to this project

---
+++ Scripts/MonoBehaviours/CardbarsScripts/EnableTextFromCardbar.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnboundLib;
+using UnityEngine;
+
+namespace CorruptedCardsManager.MonoBehaviours.CardbarsScripts {
+    public class EnableTextFromCardbar : MonoBehaviour {
+        [HideInInspector] public TextMeshProUGUI text;
+        void OnEnable() {
+            text = transform.parent.GetComponentInChildren<TextMeshProUGUI>();
+            this.ExecuteAfterFrames(1, () => {
+                text.enabled = true;
+            });
+        }
+    }
+}
---
+++ Scripts/MonoBehaviours/CardbarsScripts/GlitchedTextCardbar.cs
@@ -0,0 +1,14 @@
+﻿using UnboundLib;
+using UnityEngine;
+
+namespace CorruptedCardsManager.MonoBehaviours.CardbarsScripts {
+    [RequireComponent(typeof(EnableTextFromCardbar))]
+    public class GlitchedTextCardbar : MonoBehaviour {
+        public void Start() {
+            this.ExecuteAfterFrames(1, () => {
+                EnableTextFromCardbar enableTextFromCardbar = GetComponent<EnableTextFromCardbar>();
+                enableTextFromCardbar.text.gameObject.AddComponent<GlitchingTextMono>();
+            });
+        }
+    }
+}
